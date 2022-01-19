5616f7e97597a72f62e65a00ce10d4eeb1225cd7
lk <lakshmikumari14479@gmail.com>
2022-01-19 17:16:23 +0530

    enum

---
+++ enum/enum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace enumerators
+{
+    class Program
+{
+    enum depts
+    {
+        hr=2,salesdept=3,marketingdept
+    }
+    static void Main(string[] args)
+    {
+        int deptno = (int)depts.marketingdept;
+        Console.WriteLine(deptno);
+        Console.WriteLine();
+        Console.Read();
+    }
+}
+}
